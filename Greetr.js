64bdf05a4080c99bb2a4c0c4be845292319bc441
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2021-04-19 03:25:56 -0700

    adding comment for the code

--- Greetr.js
+++ Greetr.js
@@ -1,37 +1,48 @@
 (function(global, $) {
 
+    // 'new' an object
     let Greetr = function(firstname, lastname, language) {
         return new Greetr.init(firstname, lastname, language)
     }
     
+    // hidden within the scope of the IIFE and never directly accessble
     let supportedLanguages = ["en", "ch"]
 
+    // informal greetings
     let greetings = {
         en: "Hello",
         ch: "你好"
     };
 
+    // formal greetings
     let formalGreetings = {
         en: "Greetings",
         ch: "您好"
     };
 
+    // for login messages
     let logMessages = {
         en: "Logged in",
         ch: "已登陆"
     };
 
+    // prototype holds method to save memory space
     Greetr.prototype = {
+
+        // 'this' refers to the calling object at execution time
         fullName: function() {
             return `${this.firstname} ${this.lastname}`
         },
 
         validate: function() {
+            // check that is a valid language
+            // references the externally inaccessible 'supportedLangs' within the closure
             if (supportedLanguages.indexOf(this.language) === -1) {
                 throw "Invalid language";
             }
         },
 
+        //retrieve messages from object by referring to properties using [] syntax
         greeting: function() {
             return `${greetings[this.language]} ${this.firstname}!`;
         },
@@ -64,12 +75,17 @@
             if (console) {
                 console.log(`${logMessages[this.language]}: ${this.fullName()}`)
             };
+            //make chainable
             return this
         },
 
+        
         setLanguage: function(lang) {
+            // set the language
             this.language = lang
+            // validate
             this.validate()
+            // make chainable
             return this;
         },
 
@@ -81,6 +97,7 @@
                 throw "Missing jQuery selector";
             }
 
+            // determine the message
             let msg;
             if(formal) {
                 msg = this.formalGreeting();
@@ -89,12 +106,15 @@
                 msg = this.greeting();
             }
 
+            // inject the message in the chosen place in the DOM
             $(selector).html(msg);
 
+            // make chainble
             return this;
         }
     };
 
+    // the actual object is create here, allowing us to 'new' an object without calling 'new'
     Greetr.init = function(firstname, lastname, language) {
         let self = this;
         self.firstname = firstname;
@@ -102,8 +122,10 @@
         self. language = language || "en";
     }
 
+    // trick borrowed from jQuery so we don't have to use the 'new' keyword
     Greetr.init.prototype = Greetr.prototype;
 
+    // attach our Greetr to the global object, and provide a shorthand '$G'
     global.Greetr = global.G$ = Greetr
 
 
